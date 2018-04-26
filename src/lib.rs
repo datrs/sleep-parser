09b3c60de001b7cced23fd4a5bb5cd73a6e23540
Yoshua Wuyts <yoshuawuyts+github@gmail.com>
2018-04-26 18:56:06 +0200

    add header creation methods (#2)

--- src/lib.rs
+++ src/lib.rs
@@ -9,3 +9,18 @@ extern crate failure;
 mod header;
 
 pub use header::*;
+
+/// Create a new `Header` in the `Bitfield` configuration.
+pub fn create_bitfield() -> Header {
+  Header::new(FileType::BitField, 3328, HashType::None)
+}
+
+/// Create a new `Header` in the `Signatures` configuration.
+pub fn create_signatures() -> Header {
+  Header::new(FileType::Signatures, 64, HashType::Ed25519)
+}
+
+/// Create a new `Header` in the `Tree` configuration.
+pub fn create_tree() -> Header {
+  Header::new(FileType::Tree, 40, HashType::BLAKE2b)
+}
