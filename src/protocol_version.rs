d1c9db4d797645290d499bba1fbab486b7f7442f
Yoshua Wuyts <yoshuawuyts@gmail.com>
2018-06-15 01:55:40 +0200

    add protocol_version methods

--- src/protocol_version.rs
+++ src/protocol_version.rs
@@ -1,6 +1,14 @@
 /// SLEEP Protocol version.
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub enum ProtocolVersion {
   /// The version specified as per the paper released in 2017-09.
   V0,
+}
+
+impl ProtocolVersion {
+  /// Returns true if the version is `V0`.
+  #[inline]
+  pub fn is_v0(&self) -> bool {
+    *self == ProtocolVersion::V0
+  }
 }
